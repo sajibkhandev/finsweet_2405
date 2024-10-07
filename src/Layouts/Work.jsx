d958328c6bb41b3bdd2edd23507c6cc40eb6ed9f
sajibkhandev <sajibkhan.mern@gmail.com>
2024-10-07 11:54:20 +0600

    work part responice

--- src/Layouts/Work.jsx
+++ src/Layouts/Work.jsx
@@ -12,13 +12,13 @@ const Work = () => {
   return (
     <section className='py-[128px] bg-[#F4F6FC]'>
         <Container>
-            <Flex>
-                <div className='w-5/12 relative'>
-                <h3 className='text-5xl text-[#282938] font-semibold font-pop'>How we work</h3>
-                <p className='text-base text-[#282938] font-normal font-pop w-[405px] pt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-                <div className=' group flex items-center text-[#2405F2] text-lg font-pop font-medium absolute top-[130px] left-[0px]'><a  href="">Get in touch with us</a><FaArrowRightLong className='absolute top-[6px] left-[156px] opacity-0 group-hover:opacity-100'/></div>
+            <Flex className='!block md:!flex'>
+                <div className='md:w-5/12 relative'>
+                <h3 className='px-2.5 md:px-0  text-[28px] md:text-5xl text-[#282938] font-semibold font-pop'>How we work</h3>
+                <p className='px-2.5 md:px-0 text-sx md:text-base text-[#282938] font-normal font-pop md:w-[405px] pt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                <div className=' group flex items-center text-[#2405F2] text-lg font-pop font-medium absolute top-[130px] left-[10px] md:left-[0px]'><a  href="">Get in touch with us</a><FaArrowRightLong className='absolute top-[6px] left-[156px] opacity-0 md:group-hover:opacity-100'/></div>
                 </div>
-                <div className='w-7/12 '>
+                <div className='md:w-7/12 pt-[100px] md:pt-0'>
                 <Flex className='flex-wrap gap-y-12'>
                 <CardWork icon={Icon1} title="Strategy" dec="Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam ."/>
                 <CardWork icon={Icon2} title="Wireframing" dec="Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam ."/>
--- src/components/CardWork.jsx
+++ src/components/CardWork.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const CardWork = ({icon,title,dec}) => {
   return (
-    <div>
+    <div className=' px-2.5 md:px-0'>
         <img src={icon} alt="" />
-        <h4 className='text-[32px] text-[#282938] font-pop font-medium pt-4 pb-2'>{title}</h4>
-        <p className='text-base text-[#282938] font-pop font-normal w-[303px] '>{dec}</p>
+        <h4 className='text-6 md:text-[32px] text-[#282938] font-pop font-medium pt-4 pb-2'>{title}</h4>
+        <p className='text-sx md:text-base text-[#282938] font-pop font-normal md:w-[303px] '>{dec}</p>
 
     </div>
   )
